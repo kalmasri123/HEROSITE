45c526b6f22021ea72822ab30c6011d9afeb1ec4
kalmasri123 <kalmasri@me.com>
2018-05-23 17:27:58 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+var express= require('express');
+var path= require('path')
+var fs = require('fs')
+var app = express()
+var bodyParser=require('body-parser')
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ extended: false }));
+
+app.get('/subs.html',function(req, resp){
+console.log("receiving connection")
+resp.sendFile('subs.html',{root:__dirname})
+
+
+})
+app.get('/subs.html', function(req, res){ 
+
+res.sendFile('index.css',{root:__dirname})
+
+res.end();
+});
+app.post('/subs.html',function(req, resp){
+
+console.log(JSON.stringify(req.body))
+
+
+
+
+resp.writeHead(200, { 'Content-Type': 'text/plain' });
+
+console.log(JSON.stringify(req.body))
+
+resp.end()
+
+})
+app.listen(8080,function(){
+
+  console.log("Listening!")
+})
